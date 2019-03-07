800a93fc3f0534ea041d5ddd21f474c2898e8222
Brandon Grant <bjgrant27@gmail.com>
2019-03-07 17:12:09 +0800

    added ability to parse json data from a url

--- app/src/main/java/com/bootisdev/behappy/MainActivity.java
+++ app/src/main/java/com/bootisdev/behappy/MainActivity.java
@@ -3,11 +3,15 @@ package com.bootisdev.behappy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.bootisdev.behappy.data.QuoteData;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new QuoteData().getQuotes();
     }
 }
---
+++ app/src/main/java/com/bootisdev/behappy/data/QuoteData.java
@@ -0,0 +1,52 @@
+package com.bootisdev.behappy.data;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.bootisdev.behappy.controller.AppController;
+import com.bootisdev.behappy.model.Quote;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class QuoteData {
+    ArrayList<Quote> quoteArrayList = new ArrayList<>();
+
+    public void getQuotes() {
+        String url = "https://raw.githubusercontent.com/pdichone/UIUX-Android-Course/master/Quotes.json%20";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url,
+                new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject quoteObject = response.getJSONObject(i);
+                        Quote quote = new Quote();
+                        quote.setQuote(quoteObject.getString("quote"));
+                        quote.setAuthor(quoteObject.getString("name"));
+
+                        Log.d("STUFF: ", quoteObject.getString("name"));
+
+                        quoteArrayList.add(quote);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(jsonArrayRequest);
+    }
+}
