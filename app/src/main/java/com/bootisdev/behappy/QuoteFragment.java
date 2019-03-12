bea53351092a42ab0b98bc6869c8bac520079778
Brandon Grant <bjgrant27@gmail.com>
2019-03-12 17:19:25 +0800

    quote cards now have random background colors

--- app/src/main/java/com/bootisdev/behappy/QuoteFragment.java
+++ app/src/main/java/com/bootisdev/behappy/QuoteFragment.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,13 +35,19 @@ public class QuoteFragment extends Fragment {
         String quote = getArguments().getString("quote");
         String author = getArguments().getString("author");
 
+        int colors[] = new int[] {R.color.blue_500, R.color.pink_900, R.color.green_400,
+            R.color.lime_400, R.color.orange_400, R.color.amber_800, R.color.pink_800,
+            R.color.grey_700};
+
         quoteText.setText(quote);
         authorText.setText("-" + author);
+
+        cardView.setBackgroundResource(getRandomColor(colors));
 
         return quoteView;
     }
 
-    public static final QuoteFragment newInstance(String quote, String author) {
+    public static QuoteFragment newInstance(String quote, String author) {
         QuoteFragment fragment = new QuoteFragment();
         Bundle bundle = new Bundle();
 
@@ -51,4 +59,13 @@ public class QuoteFragment extends Fragment {
         return fragment;
     }
 
+    int getRandomColor(int[] colorArray) {
+        int color;
+
+        int randNum = ThreadLocalRandom.current().nextInt(colorArray.length);
+
+        color = colorArray[randNum];
+
+        return color;
+    }
 }
